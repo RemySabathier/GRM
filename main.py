bad6a3fbfd15d60b356a5f74bc42acb5f6ee00b5
Sabathier Remy <remy.sabathier@supelec.fr>
2021-03-28 14:37:56 +0200

    class implementation for multiple region seg

--- main.py
+++ main.py
@@ -12,27 +12,23 @@ import pickle
 
 class Features():
 
-    def __init__(self, region_array, image, model_1_path):
+    def __init__(self, superpixel_array, image, model_1_path):
 
-        self.region_array = region_array
+        self.superpixel_array = superpixel_array
         self.image = image
         self.hsv_image = rgb2hsv(self.image)
-        self.d_features = {}  # dict of features
-        self.region_prop = {}
 
         # Load the first model for similarity computation
         with open(model_1_path, 'rb') as f:
             self.model_1 = pickle.load(f)
 
-    def compute_features(self):
+    def compute_features(self, region_props):
         '''Compute the feature vector for each region'''
 
-        # Update region_prop
-        self.update_region_prop()
         # Reset the region feature vector
-        self.d_features = {}
+        d_features = {}
 
-        for region in self.region_prop:
+        for region in region_props:
 
             label = region.label
             coords = region.coords
@@ -55,73 +51,80 @@ class Features():
 
             # DOOG Filters mean abs response of 12 filters
 
-            self.d_features[label] = np.array([
+            d_features[label] = np.array([
                 mean_coords_x, mean_coords_y, mean_intensity_r, mean_intensity_g, mean_intensity_b, mean_intensity_h, mean_intensity_s, mean_intensity_v])
 
-    def update_region_prop(self):
-        self.region_prop = measure.regionprops(self.region_array)
+        return d_features
 
-    def similarity(self, reg_a, reg_b):
+    def compute_region_prop(self, array):
+        '''Compute the region properties of a labeled array'''
+        return measure.regionprops(array)
+
+    def similarity(self, reg_a, reg_b, d_features):
         '''Compute the similarity of two regions based on the feature dictionary'''
-        # It should be a learned pairwise affinity function
-        # It should be a ML model that gives a probability/similarity
         return self.model_1.predict_proba(
-            np.abs(self.d_features[reg_a]-self.d_features[reg_b]).reshape((1, -1)))[0][1]
+            np.abs(d_features[reg_a]-d_features[reg_b]).reshape((1, -1)))[0][1]
 
+    def segmentation(self, k_sel=32):
+        '''Main function to compute a region segmentation'''
+        # Boolean to monitor the termination
+        change = True
 
-def segmentation(image, k_sel=32):
-    '''Main function to compute the segmentation'''
+        # The region array : start with the initial superpixels segmentation
+        region_array = np.copy(self.superpixel_array)
 
-    # Array storing the region of pixels
-    image = np.array(image)
-    region_map = np.zeros((image.shape[0], image.shape[1]))
+        while change:
+            change = False
 
-    # First step : Generate superpixels
-    segments_sp = 1 + felzenszwalb(image, scale=100, sigma=0.1, min_size=1000)
+            # Compute the regions props
+            region_props = self.compute_region_prop(region_array)
 
-    # Instanciate Feature Object storing feature vectors of each superpixel
-    feat_store = Features(segments_sp, image, 'model_1.pk')
+            # Compute the feature dict of each region
+            d_features = self.compute_features(region_props)
 
-    # Boolean to monitor the termination
-    change = True
+            # Randomly order them and assign the first k to different regions
+            unique = np.unique(region_array)
+            np.random.shuffle(unique)
+            selected_reg, remaining_sp = unique[:k_sel], unique[k_sel:]
 
-    while change:
+            # Dict storing the new regions
+            d_new_reg = {r: r for r in selected_reg}
+            for r_sp in remaining_sp:
+                d_new_reg[r_sp] = sorted([(self.similarity(r_sp, x, d_features), x)
+                                          for x in selected_reg], key=lambda tup: tup[0])[-1][1]
 
-        change = False
+            # Update the region array with the merges
+            for k in d_new_reg:
+                if d_new_reg[k] != k:
+                    change = True
+                    region_array = np.where(
+                        region_array == k, d_new_reg[k], region_array)
 
-        # Compute the feature dict of each region
-        feat_store.compute_features()
-
-        # Randomly order them and assign the first k to different regions
-        unique = np.unique(segments_sp)
-        np.random.shuffle(unique)
-        random_reg, remaining_sp = unique[:k_sel], unique[k_sel:]
-
-        # Dict storing the new regions
-        d_new_reg = {r: r for r in random_reg}
-        for r_sp in remaining_sp:
-            d_new_reg[r_sp] = sorted([(feat_store.similarity(r_sp, x), x)
-                                      for x in random_reg], key=lambda tup: tup[0])[-1][1]
-
-        for k in d_new_reg:
-            if d_new_reg[k] != k:
-                change = True
-                segments_sp = np.where(
-                    segments_sp == k, d_new_reg[k], segments_sp)
-
-    return segments_sp
+        return region_array
 
 
 if __name__ == '__main__':
 
     # Open the image
     path_test = 'dataset/structure31.jpg'
-    image = Image.open(path_test)
+    model_1_path = 'model_1.pk'
+    image = np.array(Image.open(path_test))
 
-    # Compute the segmentation
-    X = segmentation(image, k_sel=4)
-    plt.subplot(1, 2, 1)
+    # Compute a set of superpixels
+    superpixels = 1 + felzenszwalb(image, scale=100, sigma=0.1, min_size=1000)
+
+    # Initialize the class to compute regions
+    fr = Features(superpixels, image, model_1_path)
+
+    # Compute a region segmentation with *k_sel* regions
+    X = fr.segmentation(k_sel=4)
+    X2 = fr.segmentation(k_sel=4)
+
+    # Visualization
+    plt.subplot(1, 3, 1)
     plt.imshow(image)
-    plt.subplot(1, 2, 2)
+    plt.subplot(1, 3, 2)
     plt.imshow(X)
+    plt.subplot(1, 3, 3)
+    plt.imshow(X2)
     plt.show()
