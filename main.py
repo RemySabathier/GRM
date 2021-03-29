6cc2a64f241170c2c1cdf80e4ccbb7884e546aa7
Sabathier Remy <remy.sabathier@supelec.fr>
2021-03-29 09:47:14 +0200

    region dataset generation

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage.segmentation import felzenszwalb
 from skimage.segmentation import mark_boundaries
+from skimage.color import label2rgb
 from skimage import measure
 from skimage.color import rgb2hsv
 from PIL import Image
 import random
 import scipy.io as sio
 import pickle
+from scipy.stats import entropy
 
 
 class Features():
@@ -73,34 +75,109 @@ class Features():
         # The region array : start with the initial superpixels segmentation
         region_array = np.copy(self.superpixel_array)
 
-        while change:
-            change = False
+        # Compute the regions props
+        region_props = self.compute_region_prop(region_array)
 
-            # Compute the regions props
-            region_props = self.compute_region_prop(region_array)
+        # Compute the feature dict of each region
+        d_features = self.compute_features(region_props)
 
-            # Compute the feature dict of each region
-            d_features = self.compute_features(region_props)
+        # Randomly order them and assign the first k to different regions
+        unique = np.unique(region_array)
+        np.random.shuffle(unique)
+        selected_reg, remaining_sp = unique[:k_sel], unique[k_sel:]
 
-            # Randomly order them and assign the first k to different regions
-            unique = np.unique(region_array)
-            np.random.shuffle(unique)
-            selected_reg, remaining_sp = unique[:k_sel], unique[k_sel:]
+        # Dict storing the new regions
+        d_new_reg = {r: r for r in selected_reg}
+        for r_sp in remaining_sp:
+            d_new_reg[r_sp] = sorted([(np.mean([self.similarity(r_sp, u, d_features) for u in d_new_reg if d_new_reg[u] == x]), x)
+                                        for x in selected_reg], key=lambda tup: tup[0])[-1][1]
 
-            # Dict storing the new regions
-            d_new_reg = {r: r for r in selected_reg}
-            for r_sp in remaining_sp:
-                d_new_reg[r_sp] = sorted([(self.similarity(r_sp, x, d_features), x)
-                                          for x in selected_reg], key=lambda tup: tup[0])[-1][1]
-
-            # Update the region array with the merges
-            for k in d_new_reg:
-                if d_new_reg[k] != k:
-                    change = True
-                    region_array = np.where(
-                        region_array == k, d_new_reg[k], region_array)
+        # Update the region array with the merges
+        for k in d_new_reg:
+            if d_new_reg[k] != k:
+                region_array = np.where(
+                    region_array == k, d_new_reg[k], region_array)
 
         return region_array
+
+
+class Regions():
+    '''Region class to compute region-level features'''
+
+    def __init__(self, segmentation_image, image):
+
+        self.segmentation_image = segmentation_image
+        self.image = image
+        self.hsv_image = rgb2hsv(self.image)
+
+        # compute the regions_props
+        self.region_props = self.compute_region_prop(self.segmentation_image)
+
+    def compute_region_prop(self, array):
+        '''Compute the region properties of a labeled array'''
+        return measure.regionprops(array)
+
+    def compute_region_sp_list(self, superpixel_array):
+        '''For each region, return label of superpixels inside'''
+
+        d_sp_in_r = {}
+
+        for region in self.region_props:
+
+            coords = region.coords
+            label = region.label
+            sp_labels = np.unique(
+                superpixel_array[coords[:, 0], coords[:, 1]])
+            d_sp_in_r[label] = sp_labels
+
+        return d_sp_in_r
+
+    def compute_features(self):
+        '''Compute the feature vector for each region'''
+
+
+        # Reset the region feature vector
+        d_features = {}
+
+        for region in self.region_props:
+
+            label = region.label
+            coords = region.coords
+            pix_intensity = self.image[coords[:, 0], coords[:, 1], :]
+            pix_intensity_hsv = self.hsv_image[coords[:, 0], coords[:, 1], :]
+
+            # RGB mean values
+            mean_intensity_r = np.mean(pix_intensity[:, 0])/255.
+            mean_intensity_g = np.mean(pix_intensity[:, 1])/255.
+            mean_intensity_b = np.mean(pix_intensity[:, 2])/255.
+
+            # HSV mean values
+            mean_intensity_h = np.mean(pix_intensity_hsv[:, 0])
+            mean_intensity_s = np.mean(pix_intensity_hsv[:, 1])
+            mean_intensity_v = np.mean(pix_intensity_hsv[:, 2])
+
+            # Coords mean values
+            mean_coords_x = np.mean(coords[:, 0])/self.image.shape[0]
+            mean_coords_y = np.mean(coords[:, 1])/self.image.shape[1]
+
+            # Location percentile/number
+            perc_10_coords_x = np.percentile(coords[:, 0], 10)
+            perc_10_coords_y = np.percentile(coords[:, 1], 10)
+            perc_90_coords_x = np.percentile(coords[:, 0], 90)
+            perc_90_coords_y = np.percentile(coords[:, 1], 90)
+
+            # Number of superpixels
+            nb_superpixels = region.area
+            perc_convex = region.convex_area/region.area
+
+            # DOOG Filters mean abs response of 12 filters
+
+            d_features[label] = np.array([
+                mean_coords_x, mean_coords_y, mean_intensity_r, mean_intensity_g, mean_intensity_b, mean_intensity_h, mean_intensity_s, mean_intensity_v,
+                perc_10_coords_x, perc_10_coords_y, perc_90_coords_x, perc_90_coords_y, nb_superpixels, perc_convex
+                ])
+
+        return d_features
 
 
 if __name__ == '__main__':
@@ -111,20 +188,27 @@ if __name__ == '__main__':
     image = np.array(Image.open(path_test))
 
     # Compute a set of superpixels
-    superpixels = 1 + felzenszwalb(image, scale=100, sigma=0.1, min_size=1000)
+    superpixels = 1 + felzenszwalb(image, scale=3., sigma=0.9, min_size=1000)
 
     # Initialize the class to compute regions
     fr = Features(superpixels, image, model_1_path)
 
-    # Compute a region segmentation with *k_sel* regions
-    X = fr.segmentation(k_sel=4)
-    X2 = fr.segmentation(k_sel=4)
+    # Compute a set of region segmentation with *k_sel* regions
+    seg_list = [fr.segmentation(k_sel=7) for _ in range(2)]
+
+    # Next: Create a class to compute features on region
+    reg = Regions(seg_list[0], image)
+    F = reg.compute_features()
+
+    # Estimate the likelihood of a class to be homogenous
+
+    # Estimate the class of a region1
 
     # Visualization
     plt.subplot(1, 3, 1)
-    plt.imshow(image)
+    plt.imshow(label2rgb(superpixels, image))
     plt.subplot(1, 3, 2)
-    plt.imshow(X)
+    plt.imshow(label2rgb(seg_list[0], image))
     plt.subplot(1, 3, 3)
-    plt.imshow(X2)
+    plt.imshow(label2rgb(seg_list[1], image))
     plt.show()
---
+++ train_ml region.py
@@ -0,0 +1,106 @@
+# Code to generate data for other ML model on regions
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from PIL import Image
+import random
+import scipy.io as sio
+import os
+from main import Features, Regions
+from itertools import combinations
+import random
+from tqdm import tqdm
+
+
+def load_annotation(path):
+    '''Load the Matlab annotation file and return a python dict'''
+    data = sio.loadmat(path)
+    X = data['imsegs'][0]
+    N = X.shape[0]
+    d = {}
+    for n in range(N):
+        param = {}
+        file_name = X[n][0][0]
+        param['seg_image'] = X[n][2]
+        param['npixels'] = X[n][4]
+        param['vlabels'] = X[n][6]
+        param['hlabels'] = X[n][7]
+        param['labels'] = X[n][8]
+        param['vert_labels'] = X[n][9].flatten()
+        param['horz_labels'] = X[n][10].flatten()
+        param['label_names'] = X[n][11]
+        param['vert_names'] = X[n][12]
+        param['horz_names'] = X[n][13]
+        d[file_name] = param
+    return d
+
+
+if __name__ == '__main__':
+
+    dataset_path = 'dataset'
+    model_1_path = 'model_1.pk'
+    annotation_path = r'dataset\allimsegs2.mat'
+
+    # Create a dataset of different class regions
+    # Possible Classes are : Ground, Vertical, Sky or Mixed (4)
+    # Possible Vertical Classes are: Left, Center, Right, Porous, Solid or Mixed (6)
+
+    # Step 1: Create a dataset of same-label and different-label superpixels
+    annotations = load_annotation(annotation_path)
+    N_img = len(annotations)  # Number of images
+
+    training_data_X, training_data_y = [], []
+
+    for img_path in tqdm(annotations):
+
+        image = np.array(Image.open(os.path.join(dataset_path, img_path)))
+        superpixel_array = annotations[img_path]['seg_image']
+
+        fr = Features(superpixel_array, image, model_1_path)
+
+        # Compute a set of region segmentation with *k_sel* regions
+        seg_list = [fr.segmentation(k_sel=9) for _ in range(3)]
+
+        # Next: Create a list of regions classes per proposed segmentation
+        reg_list = [Regions(seg, image) for seg in seg_list]
+
+        for regions in reg_list:
+
+            # Compute the features of all regions (for this hypothesis)
+            X_features = regions.compute_features()
+
+            # For each region, return list of superpixels inside
+            d_sp_in_r = regions.compute_region_sp_list(superpixel_array)
+
+            # We look in the annotations to find the category of each superpixels
+            for r in d_sp_in_r:
+
+                labels_id = [i-1 for i in d_sp_in_r[r]]
+
+                vertical_labels = annotations[img_path]['horz_labels'][labels_id]
+                global_labels = annotations[img_path]['vert_labels'][labels_id]
+
+                unique_global, count_global = np.unique(global_labels,return_counts=True)                
+                #If there is a main class (>80%) then tag the region with this category, otherwise tag MIX
+                if max(count_global)/sum(count_global) >= 0.8:
+                    r_global = unique_global[np.argmax(count_global)]
+                else:
+                    r_global = 3 #The MIXED label
+
+                unique_vertical, count_vertical = np.unique(vertical_labels,return_counts=True)                
+                #If there is a main class (>80%) then tag the region with this category, otherwise tag MIX
+                if max(count_vertical)/sum(count_vertical) >= 0.8:
+                    r_vertical = unique_vertical[np.argmax(count_vertical)]
+                else:
+                    r_vertical = 5 #The MIXED label
+            
+                #Add the region to the training set
+                training_data_X.append(X_features[r])
+                training_data_y.append([r_global,r_vertical])
+
+    # Save the dataset into .npy format
+    X_train = np.array(training_data_X)
+    y_train = np.array(training_data_y)
+    np.save('X_train_region.npy', X_train)
+    np.save('y_train_region.npy', y_train)
